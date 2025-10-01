87a1d6d63773191fd4e3e8010f23a5aa26cc29fa
nishhikasharma <nishhikasharma@gmail.com>
2025-10-01 08:07:16 -0700

    delete an element from an array

---
+++ day34_q68.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int n, i, pos;
+
+    scanf("%d", &n);
+    int arr[n];
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &pos);
+
+    for (i = pos; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    n--;
+
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
