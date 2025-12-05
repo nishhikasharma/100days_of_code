791f2a77cd0f782485adc22c5f2786247f077908
nishhikasharma <nishhikasharma@gmail.com>
2025-12-04 18:51:25 -0800

    Create 85.c

---
+++ 85.c
@@ -0,0 +1,35 @@
+Q85: Reverse a string.
+
+/*
+Sample Test Cases:
+Input 1:
+abcd
+Output 1:
+dcba
+*/
+
+input:
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100], rev[100];
+    int len, i, j;
+
+    // Taking input
+    printf("Enter a string: ");
+    gets(str);
+
+    len = strlen(str);
+    j = 0;
+
+    // Reversing the string manually
+    for (i = len - 1; i >= 0; i--) {
+        rev[j++] = str[i];
+    }
+    rev[j] = '\0';  // Null terminate the reversed string
+
+    printf("Reversed String: %s", rev);
+    return 0;
+}
